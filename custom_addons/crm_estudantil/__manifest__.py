0dfe4c70ea714703a638147f7ddbc84bae299765
PeterKimemiaJunior <peterkimemiajunior@gmail.com>
2026-04-27 12:06:42 +0200

    Add initial implementation of CRM Estudantil module with custom theme and website templates

--- custom_addons/crm_estudantil/__manifest__.py
+++ custom_addons/crm_estudantil/__manifest__.py
@@ -3,15 +3,15 @@
     'version': '1.0',
     'category': 'Sales/CRM',
     'summary': 'Sistema de Gestão e Acompanhamento Estudantil',
-    'description': """
-        Módulo base para o projeto de Programação Web e CMS.
-        Transforma o CRM padrão num gestor de oportunidades académicas.
-    """,
+    'description': """Módulo base.""",
     'author': 'Grupo 1 - Engenharia Informática UEM',
-    'depends': ['crm', 'website', 'survey'], # Indica que precisa destes módulos instalados primeiro
-    'data': [
-        # Aqui vão entrar os vossos ficheiros XML no futuro
-    ],
+    'depends': ['crm', 'website', 'survey'],
+    'data': [],
+    'assets': {
+        'web.assets_frontend': [
+            'crm_estudantil/static/src/css/custom_theme.scss',
+        ],
+    },
     'installable': True,
     'application': True,
 }
